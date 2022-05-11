260e43c3b96e4359c883810d8d773656c4695576
GHPrado <gghhpprado@gmail.com>
2022-05-11 14:40:10 -0300

    modifico archivos

--- contacto.js
+++ contacto.js
@@ -6,22 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
     evento.preventDefault();
     var nombre = document.getElementById('fnombre').value;
     if(nombre.length == 0) {
-      alert('No has escrito nada en el nombre');
+      alert('Debes completar el nombre');
       return;
     }
     var apellido = document.getElementById('fapellido').value;
     if(apellido.length == 0) {
-      alert('No has escrito nada en el apellido');
+      alert('Debes completar el apellido');
       return;
     }
     var telefono = document.getElementById('ftelefono').value;
     if(telefono.length == 0) {
-      alert('No has escrito nada en el telefono');
+      alert('Debes completar el telefono');
       return;
     }
     var email = document.getElementById('femail').value;
     if(email.length == 0) {
-      alert('No has escrito nada en el e-mail');
+      alert('Debes completar el e-mail');
       return;
     }
     
